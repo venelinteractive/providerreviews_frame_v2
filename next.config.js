81115dd050945e1a6f501a6329db30bdae53f217
Michael Johnson <michaelaj@gmail.com>
2024-11-07 16:27:56 -0600

    More config changes for Amplify

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
+    loader: 'custom',
+    loaderFile: './imageLoader.js',
   },
   trailingSlash: false,
   // This is important for dynamic routes in static exports
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return defaultPathMap;
-  },
+
 };
 
 module.exports = nextConfig;
